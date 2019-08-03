7724cd7647bc00230f42697e495408547d2c810c
Cmack704 <craigmack704@gmail.com>
2019-08-02 21:13:32 -0400

    finished if statement and updated database with quantity

--- cli.js
+++ cli.js
@@ -12,15 +12,15 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     start();
-    connection.end();
+    
 });
 
 function displayProducts() {
-    console.log('Coming right up...')
+    console.log('\nProducts for Sale...')
     connection.query('SELECT * FROM products', function (err, response) {
         if (err) throw err;
         for (var i = 0; i < response.length; i++) {
-            console.log('\n' + response[i].item_id + " " + response[i].product_name + " $" + response[i].price)
+            console.log('\n' + response[i].item_id + ": " + response[i].product_name + " $" + response[i].price + '\n')
         }
     })
 }
@@ -31,32 +31,58 @@ function start() {
         if (err) throw err;
         let responseArray = [];
         let priceArray = [];
+        let quantityArray = [];
         for (var i = 0; i < response.length; i++) {
             responseArray.push(response[i].item_id);
-            priceArray.push(response[i].price)
+            priceArray.push(response[i].price);
+            quantityArray.push(response[i].stock_quantity)
         }
         inquirer
-            .prompt({
+        .prompt({
                 name: 'products',
                 type: 'list',
                 message: 'What would you like to purchase?',
                 choices: responseArray
             })
-            .then(function(answer){
-                console.log(answer.products)
-
+            .then(function (answer) {
                 let productId = answer.products;
-                inquirer
+                howMany();
+                function howMany() {
+                    inquirer
                     .prompt([
-                        {
-                            name: 'howMany',
-                            type: 'input',
-                            message: 'How many would you like?'
-                        }
-                    ])
-                    .then(function(answer) {
-                        console.log('Your total is: $' + priceArray[productId - 1] * answer.howMany)
-                    })
+                            {
+                                name: 'howMany',
+                                type: 'input',
+                                message: 'How many would you like?'
+                            }
+                        ])
+                        .then(function (answer) {
+                            let quantity = quantityArray[productId - 1] - answer.howMany;
+                            function update(){
+                                connection.query("UPDATE products SET ? WHERE ?",
+                                    [
+                                        {
+                                            stock_quantity: quantity
+                                        },
+                                        {
+                                            item_id: productId
+                                        }
+                                    ],
+                                    function (err, result) {
+                                        if (err) throw err;
+                                        //console.log(result.affectedRows + ' product updated...\n');
+                                    }
+                                );
+                            };
+                            if (answer.howMany > quantityArray[productId - 1]) {
+                                howMany();
+                            } else {
+                                update();
+                                console.log('\nYour total is: $' + priceArray[productId - 1] * answer.howMany + '\n');
+                                connection.end();
+                            }
+                        })
+                }
             })
     })
 }
